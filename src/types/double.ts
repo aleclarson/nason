e252cfea5a2697dcd6415b02d51a1f6ddeab861f
Simon R <simon@reinisch.io>
2021-04-04 13:19:15 +0200

    Add double encoder

---
+++ src/types/double.ts
@@ -0,0 +1,21 @@
+export default {
+    test(v: unknown) {
+        return typeof v === 'number' && v % 1 !== 0
+    },
+
+    encode(n: number): Uint8Array {
+        const buffer = new ArrayBuffer(8)
+        const doubleView = new Float64Array(buffer)
+        doubleView[0] = n
+        return new Uint8Array(buffer)
+    },
+
+    decode(n: Uint8Array): number {
+        const buffer = new ArrayBuffer(8)
+        const doubleView = new Float64Array(buffer)
+        const uIntBuffer = new Uint8Array(buffer)
+        uIntBuffer.set(n, 0)
+
+        return doubleView[0]
+    },
+}
